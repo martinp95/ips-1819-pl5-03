3b03616df40f48b1e52e6979fff6ab2dbdbc38b7
Martin Pelaez Diaz <martinpelaezdiaz@gmail.com>
2018-10-03 12:28:00 +0200

    Initial commit.

---
+++ src/main/java/com/uniovi/Ips1819Pl503Application.java
@@ -0,0 +1,12 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ips1819Pl503Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ips1819Pl503Application.class, args);
+	}
+}
---
+++ src/test/java/com/uniovi/Ips1819Pl503ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uniovi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ips1819Pl503ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
